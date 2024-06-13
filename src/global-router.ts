d88210be2e64d2e438a9b667777f41c90d9c5e84
mrbqble <asetbek.b.1003@gmail.com>
2024-06-14 01:39:46 +0500

    first commit

---
+++ src/global-router.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import authRouter from './auth/auth-router'
+// other routers can be imported here
+
+const globalRouter = Router()
+
+globalRouter.use(authRouter)
+
+// other routers can be added here
+
+export default globalRouter
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+import { createServer } from 'node:http'
+import connectDB from './db'
+import globalRouter from './global-router'
+import { logger } from './logger'
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(logger)
+app.use('/api/v5/', globalRouter)
+
+const server = createServer(app)
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000/api/v5/')
+})
