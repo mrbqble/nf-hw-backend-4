b4069d0630dd7a1fd15a049133ae3e4c782b7d30
mrbqble <asetbek.b.1003@gmail.com>
2024-06-14 08:12:13 +0500

    boilerplate

--- src/routes/global-router.ts
+++ src/routes/global-router.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 import authRouter from './auth/auth-router'
-import s3Router from './s3/s3-router'
 // other routers can be imported here
 
 const globalRouter = Router()
 
-globalRouter.use('/s3', s3Router)
 globalRouter.use('/auth', authRouter)
 // other routers can be added here
 
